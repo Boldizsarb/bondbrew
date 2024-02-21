328d092dfad53d4dc04d4fe6c571c34598419dd8
Boldizsarb <donboyszy@gmail.com>
2024-02-21 21:03:24 +0000

    info card done

--- clientside/src/components/profilSite/profileLeftSide/infoCardOnTheLeftSide/infoCard.jsx
+++ clientside/src/components/profilSite/profileLeftSide/infoCardOnTheLeftSide/infoCard.jsx
@@ -9,10 +9,15 @@ import { useParams } from "react-router-dom";
 import * as UserApi from "../../../../api/userRequest";
 import { logout } from "../../../../actions/signupAction";
 import { followUser, unfollowUser } from "../../../../actions/userAction";
+import dummyUser from "./dummyUser.json"; // this is the dummy user, it is used to prevent the app from crashing when the person is not defined
 
-/// will need to make the data dynamic here as well 
 
 const InfoCard = ({location,person}) => {
+
+  //console.log(location)
+  if (!person) {
+    person = dummyUser;
+  }
 
   const [modalOpened, setModalOpened] = useState(false); // this is for the modal of the info card, it is false by default it is closed 
   const dispatch = useDispatch()
@@ -21,10 +26,7 @@ const InfoCard = ({location,person}) => {
   const [profileUser, setprofileUser] = useState({}); 
   const {user} = useSelector((state) => state.authReducer.authData); // current user from the store 
 
-  // const storeUser = useSelector((state) => state.authReducer.authData.user);
-  // const user = location === "clickedProfile" ? (person || storeUser) : storeUser; // this is the reason why the pen is in there 
-
-
+  
   useEffect(() => {
     const fetchProfileUser = async () => {
       if (currentUserId === user._id) {
@@ -32,25 +34,40 @@ const InfoCard = ({location,person}) => {
        // console.log(profileUser)
       } else {
         //console.log("loading")
+        if (location === "clickedProfile") { // if the request is coming from the clicked user, then the person is defined hence the person's data is used
+          setprofileUser(person);
+          return;
+        }else{
         const profileUser = await UserApi.getUser(currentUserId);
         
         setprofileUser(profileUser);
+        }
         //console.log(profileUser)
       }
     };
     fetchProfileUser();
-  }, [user]); // to prevent the infinite loop 
+  }, [currentUserId, user, person, location]); // to prevent the infinite loop  
 
   const handleLogOut = ()=> {
     dispatch(logout())
   }
 
-  console.log(person)
-  // follow button: 
-  // const [following, setFollowing] = useState(   // this brakes because the person is not defined here, need a dummy user, 
-  //   person.followers.includes(user._id)
-    
-  // );
+  //console.log(person)
+
+  //follow button: 
+  const [following, setFollowing] = useState(false);
+
+  useEffect(() => {
+    setFollowing(person.followers?.includes(user._id));
+  }, [person, user._id]);
+
+  const handleFollow = () => {
+    following
+      ? dispatch(unfollowUser(person._id, user))
+      : dispatch(followUser(person._id, user));
+    setFollowing((prev) => !prev);
+  };
+  
 
 
 
@@ -94,9 +111,20 @@ const InfoCard = ({location,person}) => {
         </span>
         <span>{profileUser.worksAt}</span>
       </div>
-      
-
-      <button className="button logout-button" onClick={handleLogOut}>Logout</button>
+      {location === "clickedProfile" ? (
+        <button
+          className={
+            following ? "button fc-button UnfollowButton" : "button fc-button"
+          }
+          onClick={handleFollow}
+        >
+          {following ? "Unfollow" : "Follow"}
+        </button>
+      ) : (
+        <button className="button logout-button" onClick={handleLogOut}>
+          Logout
+        </button>
+      )}
     </div>
   );
 };
