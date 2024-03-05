e3d0dd7fa7143af5d362418f5c34312d0a71c0b1
Boldizsarb <donboyszy@gmail.com>
2024-03-05 20:31:28 +0000

    ad

--- clientside/src/pages/matching/matching.jsx
+++ clientside/src/pages/matching/matching.jsx
@@ -8,29 +8,6 @@ import RightArrow from '../../img/arrow_right.png';
 
 
 
-const db = [
-    {
-      name: 'Richard Hendricks',
-      url: './img/richard.jpg'
-    },
-    {
-      name: 'Erlich Bachman',
-      url: './img/erlich.jpg'
-    },
-    {
-      name: 'Monica Hall',
-      url: './img/monica.jpg'
-    },
-    {
-      name: 'Jared Dunn',
-      url: './img/jared.jpg'
-    },
-    {
-      name: 'Dinesh Chugtai',
-      url: './img/dinesh.jpg'
-    }
-  ]
-
 
 const Matching = () => {    
 
@@ -38,14 +15,57 @@ const Matching = () => {
     const [genderedUsers, setGenderedUsers] = useState(null)
     const [lastDirection, setLastDirection] = useState()
     const [isMatchesVisible, setIsMatchesVisible] = useState(false);
+    const [characters, setCharacters] = useState([]) // array of all people
+    const getUserUrl = process.env.REACT_APP_AXIOS_BASE_URL;
+    const serverPubicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
 
-    const characters = db
- 
+    const getCharacters = useEffect(() => {
+        const getCharacters = async () => {
+          try {
+            const response = await fetch(`${getUserUrl}userser/`);
+            if (!response.ok) {
+              throw new Error(response.statusText)
+            }
+            const data = await response.json();
+            //const reversedData = data.reverse(); // reverse the data to get the last user first
+            setCharacters(data);
+          } catch (error) {
+            console.log(error.message);
+          }
+        }
+        getCharacters(); // gets called when the page is loaded
+    }, []);
+    console.log(characters)
+
+
+
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentCharacter, setCurrentCharacter] = useState({})
+
+    useEffect(() => {     // this is all the indexing and it is fucked right now, need to solve the indexign 
+      console.log("Current Index:", currentIndex);
+      console.log("Current Character:", currentCharacter);
+  }, [currentIndex, currentCharacter]);
+
+
+  useEffect(() => {
+    // Update currentCharacter based on currentIndex
+    if (characters.length > 0 && currentIndex >= 0) {
+        setCurrentCharacter(characters[currentIndex]);
+    }
+}, [currentIndex, characters]);
+
   
-    const swiped = (direction, nameToDelete) => {
+    const swiped = (direction, nameToDelete, index) => {
       console.log('removing: ' + nameToDelete)
       setLastDirection(direction)
+      //setCurrentIndex(index + 1);
+      setCurrentIndex(prevIndex => prevIndex + 1);
+     
+   
     }
+    //console.log(currentCharacter)
   
     const outOfFrame = (name) => {
       console.log(name + ' left the screen!')
@@ -55,7 +75,9 @@ const Matching = () => {
     const toggleMatches = () => {
         setIsMatchesVisible(!isMatchesVisible); // Toggle visibility state
     };
+    
 
+    
 
 
     return (
@@ -74,13 +96,16 @@ const Matching = () => {
                     </div>
                 <div className='cardContainer'>
 
-                    {characters.map((character) =>
-                        <TinderCard className='swipe' key={character.name}
-                         onSwipe={(dir) => swiped(dir, character.name)} 
-                         onCardLeftScreen={() => outOfFrame(character.name)}>
+                {characters.slice().reverse().map((character, index) =>
+                        <TinderCard className='swipe' key={character.firstname}
+                          //onSwipe={(dir) => swiped(dir, character.firstname)} 
+                          onSwipe={(dir) => swiped(dir, character.firstname, index)}
+                         
+                         onCardLeftScreen={() => outOfFrame(character.firstname)}>
 
-                            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
-                                <h3>{character.name}</h3>
+                              <div style={{ backgroundImage: 'url(' + (character.profilePicture ? 
+                                serverPubicFolder + character.profilePicture : serverPubicFolder + "defaultProfile.png") + ')' }} className='card'>
+                                <h3 style={{color:"var(--buttonHover)"}}>{character.firstname} {character.lastname}</h3>
                             </div>
                         </TinderCard>
                     )}
@@ -95,8 +120,9 @@ const Matching = () => {
             </div>
 
 
-
-            <h1>Matching</h1>
+          
+            <p>{currentCharacter.firstname}</p> 
+          
         </div>
         </>
     )
@@ -105,3 +131,6 @@ const Matching = () => {
 
 
 export default Matching;
+
+
+// {characters.slice().reverse().map((character, index) => // the slice and reverse is to reverse the array and then map it, but it wasnt there 
