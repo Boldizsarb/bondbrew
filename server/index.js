215630c7c79d2ab7dca3038ce5fa9ba1ee68253f
Boldizsarb <donboyszy@gmail.com>
2024-04-12 16:46:06 +0100

    a

--- server/index.js
+++ server/index.js
@@ -31,17 +31,26 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 //app.use(cors()); // original
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = ['http://176.58.109.95', 'http://localhost:3000', 'http://localhost5000']; // Add more domains as needed
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS not allowed from this origin'));
-    }
-  },
+// const corsOptions = { // specific cors options
+//   origin: function (origin, callback) {
+//     const allowedOrigins = ['http://176.58.109.95', 'http://localhost:3000', 'http://localhost:5000',
+//   'http://server:5000', 'http://frontend:3000']; // Add more domains as needed
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('CORS not allowed from this origin'));
+//     }
+//   },
+//   optionsSuccessStatus: 200
+// };
+
+const corsOptions = { 
+  origin: "*", // Allow all origins
   optionsSuccessStatus: 200
 };
+app.use(cors(corsOptions));
+
+
 app.use(cors(corsOptions));
 
 
