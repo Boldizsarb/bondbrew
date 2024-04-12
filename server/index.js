50d3d9ad8d6c013adaac299ebd4ff6c7e9c60106
Boldizsarb <donboyszy@gmail.com>
2024-04-12 16:31:51 +0100

    a

--- server/index.js
+++ server/index.js
@@ -28,7 +28,21 @@ app.use("/images", express.static("images"));
 // Middleware
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors());
+
+//app.use(cors()); // original
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = ['http://176.58.109.95', 'http://localhost:3000', 'http://localhost5000']; // Add more domains as needed
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed from this origin'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 
 // Routes
